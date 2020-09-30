65a2a1ddd3f7ce4960d8b700518c0158fa01abf3
Oleksandr Dievri <o.dievri@gmail.com>
2020-09-30 14:05:27 +0300

    fix spacing

--- commands.lua
+++ commands.lua
@@ -1,12 +1,12 @@
 minetest.register_chatcommand("ls", {
     func = function(name, params)
-        local response = ls(name, "ls")
+        local response = ls(name, "ls " .. params)
         return true, response
     end
 })
 
 minetest.register_on_chat_message(function(name, message)
-    if string.match(message, "^ls ") then
+    if string.match(message, "^ls") then
         local response = ls(name, message)
         minetest.chat_send_player(name, response)
         return true
--- help_func.lua
+++ help_func.lua
@@ -4,5 +4,5 @@ ls = function(name, message)
     local node_pos = minetest.find_node_near(pos, 6, {"mine9:platform"})
     local host_info = platforms.get_host_info(node_pos)
     local result = platforms.execute_cmd(host_info, config.lcmd, message .. " " .. host_info["path"])
-    return result
+    return result .. "\n"
 end
