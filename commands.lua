38a61ffa5d3a0b2b0d408ff66dc424e6e6fa3cbc
Oleksandr Dievri <o.dievri@gmail.com>
2020-09-30 12:51:22 +0300

    implement /ls and ls using 9p read

--- commands.lua
+++ commands.lua
@@ -1,6 +1,14 @@
 minetest.register_chatcommand("ls", {
-    func = function(name, params) 
+    func = function(name, params)
         local response = ls(name)
         return true, response
     end
 })
+
+minetest.register_on_chat_message(function(name, message)
+    if message == "ls" then
+        local response = ls(name)
+        minetest.chat_send_player(name, response)
+        return true
+    end
+end)
