90326bab95633268e3010571eed58627a888a11b
Oleksandr Dievri <o.dievri@gmail.com>
2020-10-08 13:06:02 +0300

    handle path without trailing slash

--- commands.lua
+++ commands.lua
@@ -35,16 +35,33 @@ minetest.register_chatcommand("whereis", {
 minetest.register_on_chat_message(function(name, message)
     if string.match(message, "^cp") or string.match(message, "^mv") then
         local cmd, src, dst = parse_cmd_src_dst(message)
-        local ent_pos, entity, file_name, type, src_path =
-            get_src_entity(name, src)
-        if not entity then
-            minetest.chat_send_player(name, "no matching entity")
+
+        -- check source
+        local src_path, file_type, file_name = get_src_path_type(name, src)
+        if not src_path then
+            minetest.chat_send_player(name, "no source found")
             return true
         end
-        local plt_pos = dst_exists(dst, src_path, type)
+
+        -- check destination
+        local plt_pos = dst_exists(dst, src_path, file_type)
+        if not plt_pos then
+            minetest.chat_send_player(name, "no destination spawned")
+            return true
+        end
+
+        local entity, ent_pos = get_src_entity(name, file_name)
+
         local dst_pos = get_dst_pos(plt_pos, file_name)
+
         if dst_pos ~= nil then
             print("animate file")
+
+            local direction = vector.direction(ent_pos, dst_pos)
+            local fast_direction = vector.multiply(direction, 50)
+
+            entity:set_velocity({x = 0, y = 10, z = 0})
+            entity:set_acceleration(fast_direction)
         else
             print("process file in place")
         end
--- help_func.lua
+++ help_func.lua
@@ -18,46 +18,89 @@ parse_cmd_src_dst = function(message)
     return cmd, src, dst
 end
 
-get_src_entity = function(name, src)
+get_src_path_type = function(name, src)
+    -- get platform path
+    local node_pos = nmine.node_pos_near(name)
+    local src_path = platforms.storage_get(node_pos, "host_info").path
+
+    -- get source file type
+    local type = nil
+    local listing = platforms.storage_get(node_pos, "listing")
+    local file_name = string.match(string.match(src, "/?%a+/?$"), "%a+")
+    if file_name ~= nil then
+        type = listing[file_name] ~= nil and listing[file_name].type
+    end
+    if not type then return nil end
+    return src_path, type, file_name
+end
+
+get_src_entity = function(name, file_name)
     local node_pos = nmine.node_pos_near(name)
     local listing = platforms.storage_get(node_pos, "listing")
-    local src_path = platforms.storage_get(node_pos, "host_info").path
-    local file_name = string.match(string.match(src, "/?%a+/?$"), "%a+")
-    if file_name == nil then return end
-    if listing[file_name] ~= nil then
-        local p = listing[file_name].pos
-        p.y = listing[file_name].pos.y + 1.5
-        local entity = minetest.get_objects_inside_radius(p, 1)[1]
-        return listing[file_name].pos, entity, file_name,
-               listing[file_name].type, src_path
-    else
-        return
-    end
+    local p = listing[file_name].pos
+    p.y = listing[file_name].pos.y + 1.5
+    local entity = minetest.get_objects_inside_radius(p, 1)[1]
+    return entity, p
+
 end
 
 dst_exists = function(dst, src_path, type)
-    local dst_dir = nil
+    local sd_platforms = minetest.deserialize(sd:get_string("platforms"))
+    local dst_dir = src_path
     local path = dst
 
+    -- if destination is relational, append source path
     if not string.match(dst, "^/") then
         path = src_path == "/" and src_path .. dst or src_path .. "/" .. dst
     end
+
+    -- if destination is root
     if string.match(path, "^/$") then
-        local sd_platforms = minetest.deserialize(sd:get_string("platforms"))
         for _, plt_pos in pairs(sd_platforms) do
-            local host_info = platforms.storage_get(plt_pos, "host_info")
-            if host_info.path == "/" then return plt_pos end
+            if platforms.storage_get(plt_pos, "host_info").path == "/" then
+                return plt_pos
+            end
         end
     end
-
+    -- if trailing shash, remove
     if string.match(path, "/$") then dst_dir = string.match(path, ".*[^/]") end
+
+    -- if path ends with name, check is exists and is it dir or file
     if string.match(path, "/%a+$") then
-        dst_dir = string.match(string.match(path, ".*/"), ".*[^/]")
+        local parent_dst_dir = string.match(path, "^/%a+$") and "/" or string.match(string.match(path, ".*/"), ".*[^/]")
+        for _, plt_pos in pairs(sd_platforms) do
+            if platforms.storage_get(plt_pos, "host_info").path ==
+                parent_dst_dir then
+                local dst_dir_or_file_name = string.match(path, "%a+$")
+                local dst_dir_or_file =
+                    platforms.storage_get(plt_pos, "listing")[dst_dir_or_file_name]
+                -- if dst is not exist, create file in parent directory
+
+                if (not dst_dir_or_file) then
+                    dst_dir = parent_dst_dir
+                    return dst_dir
+                end
+
+                if dst_dir_or_file.type == 128 then
+                    dst_dir = path
+                end
+
+                -- cant'move dir to file
+                if type == 128 and dst_dir_or_file.type ~= 128 then
+                    dst_dir = nil
+                    return
+                end
+
+                if type ~= 128 and dst_dir_or_file.type ~= 128 then
+                    dst_dir = parent_dst_dir
+                end
+            end
+        end
     end
-    local sd_platforms = minetest.deserialize(sd:get_string("platforms"))
     for _, plt_pos in pairs(sd_platforms) do
-        local host_info = platforms.storage_get(plt_pos, "host_info")
-        if host_info.path == dst_dir then return plt_pos end
+        if platforms.storage_get(plt_pos, "host_info").path == dst_dir then
+            return plt_pos
+        end
     end
 end
 
